0f58e622a132a9fab5fa5aae64ebbc8b7160aec0
Cruze <Rickard.bohman@iths.se>
2022-10-28 18:09:51 +0200

    Fixat alla fel som nämndes i min återkoppling för lab 2!

--- ShapesLibary/Circle.cs
+++ ShapesLibary/Circle.cs
@@ -1,71 +1,40 @@
-﻿using ShapesLibary;
-using System.Numerics;
+﻿using System.Numerics;
 
-public class Circle : Shape2D
+namespace ShapesLibary
 {
-
-    //Skapa private properties och sedan feeders till dom.
-    //Sedan gör du uträkingen.
-
-    private Vector2 _center;
-    private float _radius;
-
-    public float areaRadius;
-
-    public Circle(Vector2 center, float radius)
+    public class Circle : Shape2D
     {
-        _center = center;
-        _radius = radius;
-        areaRadius = radius;
+
+        //Skapa private properties och sedan feeders till dom.
+        //Sedan gör du uträkingen.
+
+        private Vector2 _center;
+        private float _radius;
+
+        public float areaRadius;
+
+        public Circle(Vector2 center, float radius)
+        {
+            _center = center;
+            _radius = radius;
+            areaRadius = radius;
+        }
+
+
+        public override float Circumference => (float)Math.PI * areaRadius * 2;
+
+        public override Vector3 Center => new Vector3(Center.X, Center.Y, 0f);
+
+        public override float Area => (float)Math.PI * areaRadius * areaRadius;
+
+        public override string ToString() => $"circle @{_center.ToString("f2")}: r ={_radius.ToString("f2")}";
+
     }
 
-
-    public override float Circumference => (float)Math.PI * areaRadius * 2;
-
-    public override Vector3 Center => new Vector3(Center.X, Center.Y, 0f);
-
-    public override float Area => (float)Math.PI * areaRadius * areaRadius;
-
-    public override string ToString() => $"circle @{_center.ToString("f2")}: r ={_radius.ToString("f2")}";
-
-
-
-        
-
-    
-
-    //----------Första {} ska vara cirkeln centerpunkt och andra ska vara radien---------
-    
 }
 
 
-/*
-//Arean = pi * radien * radien 
 
-private float _getArea()
-{
-    Vector2 CirlceArea = new Vector2(Area, Area);
-
-    float radius = CirlceArea.Length();
-
-    float Area = radius * radius * 3.14f;
-
-    return Area;
-}
-
-//Omkretsen = pi * diametern , (radien * 2)
-
-private float _getCircumference(float Circumference)
-{
-    Vector2 radius = new Vector2(Circumference, Circumference);
-
-    float diameter = radius.Length() * 2;
-
-    Circumference = diameter * 3.14f;
-
-    return Circumference;
-}
-*/
 
 
 
--- ShapesLibary/Shape.cs
+++ ShapesLibary/Shape.cs
@@ -18,34 +18,34 @@ namespace ShapesLibary
                 {
                     case 0:
                         Vector2 center0 = new Vector2(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
-                        float radius0 = Random.Shared.NextSingle() * 200 - 100;
+                        float radius0 = Random.Shared.NextSingle() * 100;
 
                         return new Circle(center0, radius0);
 
 
                     case 1:
                         Vector2 center1 = new Vector2(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
-                        Vector2 size1 = new Vector2(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
+                        Vector2 size1 = new Vector2(Random.Shared.NextSingle() * 100, Random.Shared.NextSingle() * 100);
 
                         return new Rectangle(center1, size1);
 
                     case 2:
                         Vector2 center2 = new Vector2(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
-                        Vector2 size2 = new Vector2(Random.Shared.NextSingle() * 200 - 100);
+                        Vector2 size2 = new Vector2(Random.Shared.NextSingle() * 100);
 
                         return new Rectangle(center2, size2);
 
 
                     case 3:
                         Vector3 center3 = new Vector3(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
-                        Vector3 size3 = new Vector3(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
+                        Vector3 size3 = new Vector3(Random.Shared.NextSingle() * 100, Random.Shared.NextSingle() * 100, Random.Shared.NextSingle() * 100);
 
                         return new Cuboid(center3, size3);
 
 
                     case 4:
                         Vector3 center4 = new Vector3(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
-                        Vector3 size4 = new Vector3(Random.Shared.NextSingle() * 200 - 100);
+                        Vector3 size4 = new Vector3(Random.Shared.NextSingle() * 100);
 
                         return new Cuboid(center4, size4);
 
@@ -53,7 +53,7 @@ namespace ShapesLibary
 
                     case 5:
                         Vector3 center5 = new Vector3(Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100, Random.Shared.NextSingle() * 200 - 100);
-                        float radius5 = Random.Shared.NextSingle() * 200 - 100;
+                        float radius5 = Random.Shared.NextSingle() * 100;
 
                         return new Sphere(center5, radius5);
 
